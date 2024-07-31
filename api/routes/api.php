b0aeab375a8972e96ca5b75252eb7b98d533c776
marihab <marihabade@gmail.com>
2024-07-31 17:41:20 +0100

    ajout de react.blade.php

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function (Request $request) {
+    return "bonjour";
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
 
 Route::get('/creer_admin',[UserController::class, 'creer_admin']);
--- api/routes/web.php
+++ api/routes/web.php
@@ -1,23 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-
-});
-Route::get('/creer_admin',[UserController::class, 'creer_admin']);
 
 Route::get('/{any}', function () {
     return view('react');
