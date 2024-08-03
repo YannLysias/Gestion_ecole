c60b8449f8fab578cf1e44c9a666222fcc1938bb
marihab <marihabade@gmail.com>
2024-08-03 17:01:10 +0100

    files setup

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\UtilisateurController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,8 +10,10 @@ Route::get('/', function (Request $request) {
     return "bonjour";
 });
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
+
+Route::apiResource('utilisateurs',UtilisateurController::class);
 Route::get('/creer_admin',[UserController::class, 'creer_admin']);
